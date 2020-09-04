607e2d0f2fb7a5cfaadc5e52a0cdb106ac977946
Jacob Ismael <jacob.ismael@gmail.com>
2020-09-04 00:35:24 -0700

    commit message

--- bot.py
+++ bot.py
@@ -9,41 +9,34 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='wire.')
-bot.remove_command("help")
+bot = commands.Bot(command_prefix='>')
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game('>help'))
     print("Wire is active")
 
-
-@bot.command()
+@bot.command(name="clr", help='cleares the specified messages')
 @commands.has_role('admin')
-async def logout(ctx):
-    await ctx.send("```disconnecting to Wire```")
-    await bot.close()
-
-@bot.command()
-@commands.has_role('admin')
-async def clear(ctx, amount=5):
+async def clr(ctx, amount=5):
     if(amount == -1):
         amount = 1000000000
     await ctx.channel.purge(limit=amount+1)
 
+bot.remove_command("help")
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title="Wire Commands", description="List of Wire commands", color=0xffff00)
-    embed.set_thumbnail(url='https://media.discordapp.net/attachments/714340123327594511/726354184886026270/wire.png')
-    embed.set_author(name="Wire", icon_url='https://media.discordapp.net/attachments/714340123327594511/726354184886026270/wire.png')
+    embed.set_thumbnail(url='https://icons.iconarchive.com/icons/xenatt/the-circle/512/App-Terminal-icon.png')
+    embed.set_author(name="Wire", icon_url='https://icons.iconarchive.com/icons/xenatt/the-circle/512/App-Terminal-icon.png')
     embed.add_field(name="Admin Commands", value=" - `mkdir`\tcreates a new channel\n - `rmdir`\tdeletes the channel you are currently in\n - `clear`\tclears a specified amount of messages", inline=False)
     embed.add_field(name="General Commands", value=" - `help`\tShows this message\n - `ls`\tlists the members of the server\n - `info`\tshows about info", inline=False)
     await ctx.send(embed=embed)
 
 
-@bot.command(name='mkdir', help='creates a new channel')
+@bot.command(name='cc', help='creates a new channel')
 @commands.has_role('admin')
-async def mkdir(ctx, channel_name='new-channel'):
+async def rc(ctx, channel_name='new-channel'):
     server = ctx.message.guild
     existing_channel = discord.utils.get(server.channels, name=channel_name)
     if not existing_channel:
@@ -52,11 +45,25 @@ async def mkdir(ctx, channel_name='new-channel'):
     else:
         await ctx.send("```Error: Channel with same name already exists```")
 
-@bot.command(help='deletes the current channel you are in')
+@bot.command(name="rc", help='deletes the current channel you are in')
 @commands.has_role('admin')
-async def rmdir(ctx):
+async def rc(ctx):
     await ctx.channel.delete()
 
+@bot.command(name='kick')
+@commands.has_role('admin')
+async def kick(ctx, member : discord.Member, reason=None):
+    await member.kick(reason=reason)
+    ctx.send("Kicked " + member)
+
+@bot.command(name='ban')
+@commands.has_role('admin')
+async def ban(ctx, member : discord.Member, reason=None):
+    await member.ban(reason=reason)
+    ctx.send("Banned " + member)
+        
+
+# Peasant Commands
 
 @bot.command(help="lists the members of the server")
 async def ls(ctx):
@@ -68,9 +75,8 @@ async def ls(ctx):
 @bot.command(help='display about info')
 async def info(ctx):
     embed = discord.Embed(title="About Wire", description="Wire is a discord bot created by [Jacob Ismael](https://github.com/jacobismael)", color=0xffff00)
-    embed.set_image(url='https://code.visualstudio.com/assets/docs/editor/integrated-terminal/integrated-terminal.png')
     embed.set_thumbnail(url='https://avatars1.githubusercontent.com/u/48426248?s=460&u=82650bc8f2dcb28c15f168c14d06833089720afa&v=4')
-    embed.set_author(name="Wire", icon_url='https://media.discordapp.net/attachments/714340123327594511/726354184886026270/wire.png')
+    embed.set_author(name="Wire", icon_url='https://icons.iconarchive.com/icons/xenatt/the-circle/512/App-Terminal-icon.png')
 
     embed.set_footer(text='Wire v1.0')
     await ctx.send(embed=embed)
